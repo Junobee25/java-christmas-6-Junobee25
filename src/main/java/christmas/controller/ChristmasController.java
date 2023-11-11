b27fb71ad2dbc5eb845093841c91162371d36cbe
Junobee25 <wnsdhqo@naver.com>
2023-11-12 00:15:07 +0900

    feat(controller): inputOrder 메서드 구현

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,26 +1,29 @@
 package christmas.controller;
 
 import christmas.domain.Date;
+import christmas.domain.Order;
 import christmas.service.ChristmasService;
 import christmas.util.Utils;
 import christmas.view.InputView;
 import christmas.view.OutputView;
+
+import java.util.Map;
 
 public class ChristmasController {
     private final ChristmasService christmasService;
 
     public ChristmasController() {
         christmasService = new ChristmasService();
-        int date = inputDate();
     }
 
     public void decemberPromotion() {
-
+        int date = inputDate();
     }
 
     private int inputDate() {
         OutputView.outputViewWelcome();
         InputView.inputViewDate();
+
         while (true) {
             try {
                 Date date = new Date(getInputDate());
@@ -31,8 +34,26 @@ public class ChristmasController {
         }
     }
 
+    public Map<String, Integer> inputOrder() {
+        InputView.inputViewOrder();
+
+        while (true) {
+            try {
+                Order order = new Order(getInputOrder());
+                return order.getOrder();
+            } catch (IllegalArgumentException e) {
+
+            }
+        }
+
+    }
+
     private int getInputDate() {
         String userInput = InputView.input().trim();
         return Utils.stringToInteger(userInput);
+    }
+
+    private Map<String, Integer> getInputOrder() {
+        return Utils.stringToMap(InputView.input().trim());
     }
 }
