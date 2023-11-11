6afaf5a553843efa6dfd313d9b0fc5068c5f60a4
Junobee25 <wnsdhqo@naver.com>
2023-11-11 21:43:49 +0900

    feat(InputView): 입력 문자열 상수 설정

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,12 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private final static String EXPECTED_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private final static String SELECT_MENU = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+
+    public static String input() {
+        return Console.readLine();
+    }
+}
