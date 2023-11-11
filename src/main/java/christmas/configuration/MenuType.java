4bcb37ccc6d6f501ba9682e953a51bedb687762d
Junobee25 <wnsdhqo@naver.com>
2023-11-11 23:51:09 +0900

    feat(configuration): 전체 메뉴 타입 Enum 생성

---
+++ src/main/java/christmas/configuration/MenuType.java
@@ -0,0 +1,14 @@
+package christmas.configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum MenuType {
+    MENU(Arrays.asList("양송이수프","타파스","시저샐러드","초코케이크","아이스크림","티본스테이크","바비큐립","해산물파스타","크리스마스파스타"));
+
+    private final List<String> menu;
+
+    MenuType(List<String> menu) {
+        this.menu = menu;
+    }
+}
