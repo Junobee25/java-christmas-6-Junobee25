331be828b2a4e7c19d71544ee88fd859938031f3
Junobee25 <wnsdhqo@naver.com>
2023-11-11 18:27:27 +0900

    fix(DiscountType): 크리스마스 할인 정책 계산식 수정

--- src/main/java/christmas/configuration/DiscountType.java
+++ src/main/java/christmas/configuration/DiscountType.java
@@ -5,9 +5,9 @@ import java.util.List;
 import java.util.function.Function;
 
 public enum DiscountType {
-    WEEKDAY(Arrays.asList(4, 5, 6, 7, 11, 12, 13, 14, 18, 19, 20, 21, 25, 26, 27, 28), dessertPrice -> dessertPrice - 2023),
-    WEEKEND(Arrays.asList(1, 2, 8, 9, 15, 16, 22, 23, 29, 30), mainPrice -> mainPrice - 2023),
-    CHRISTMAS(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 29, 20, 21, 22, 23, 24, 25), date -> 100 * (date - 1));
+    WEEKDAY(Arrays.asList(4, 5, 6, 7, 11, 12, 13, 14, 18, 19, 20, 21, 25, 26, 27, 28), value -> value - 2023),
+    WEEKEND(Arrays.asList(1, 2, 8, 9, 15, 16, 22, 23, 29, 30), value -> value - 2023),
+    CHRISTMAS(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 29, 20, 21, 22, 23, 24, 25), value -> 1000 + 100 * (value - 1));
 
     private final List<Integer> dates;
     private final Function<Integer, Integer> expression;
@@ -15,6 +15,14 @@ public enum DiscountType {
     DiscountType(List<Integer> dates, Function<Integer, Integer> expression) {
         this.dates = dates;
         this.expression = expression;
+    }
+
+    public List<Integer> getDates() {
+        return dates;
+    }
+
+    public int calculate(int value) {
+        return expression.apply(value);
     }
 
 }
