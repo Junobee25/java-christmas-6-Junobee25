226579e8a27a6ca9df13f3efe999937857322016
Junobee25 <wnsdhqo@naver.com>
2023-11-11 17:21:38 +0900

    feat(DiscountType): 할인 정책 Enum 생성

---
+++ src/main/java/christmas/configuration/DiscountType.java
@@ -0,0 +1,20 @@
+package christmas.configuration;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+public enum DiscountType {
+    WEEKDAY(Arrays.asList(4, 5, 6, 7, 11, 12, 13, 14, 18, 19, 20, 21, 25, 26, 27, 28), value -> value - 2023),
+    WEEKEND(Arrays.asList(1, 2, 8, 9, 15, 16, 22, 23, 29, 30), value -> value - 2023),
+    CHRISTMAS(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 29, 20, 21, 22, 23, 24, 25), value -> 100 * (value - 1));
+
+    private final List<Integer> dates;
+    private final Function<Integer, Integer> expression;
+
+    DiscountType(List<Integer> dates, Function<Integer, Integer> expression) {
+        this.dates = dates;
+        this.expression = expression;
+    }
+
+}
