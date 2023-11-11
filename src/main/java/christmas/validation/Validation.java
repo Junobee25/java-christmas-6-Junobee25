4e0fa72bec33957bf8aaa2f5ea80959736e57c44
Junobee25 <wnsdhqo@naver.com>
2023-11-11 22:21:00 +0900

    feat(validation): validateEmptyInputDate 메서드 구현

--- src/main/java/christmas/validation/Validation.java
+++ src/main/java/christmas/validation/Validation.java
@@ -3,8 +3,15 @@ package christmas.validation;
 import christmas.view.ErrorMessage;
 
 public class Validation {
-    public static void validateStringToInteger(String string) {
-        if (!string.chars().allMatch(Character::isDigit)) {
+    public static void validateEmptyInputDate(String userInput) {
+        if (userInput.length() == 0) {
+            ErrorMessage.inputDateError();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void validateStringToInteger(String userInput) {
+        if (!userInput.chars().allMatch(Character::isDigit)) {
             ErrorMessage.inputDateError();
             throw new IllegalArgumentException();
         }
