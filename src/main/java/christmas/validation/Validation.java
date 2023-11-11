2d10d3fd97887856c3b8953b90a7a8773335f5ca
Junobee25 <wnsdhqo@naver.com>
2023-11-11 23:34:05 +0900

    feat(validation): validateFoodArrayElement 메서드 구현

--- src/main/java/christmas/validation/Validation.java
+++ src/main/java/christmas/validation/Validation.java
@@ -5,6 +5,7 @@ import christmas.view.ErrorMessage;
 public class Validation {
     private final static String REST = ",";
     private final static String DASH = "-";
+    private final static int ELEMENT_LENGTH = 3;
 
     public static void validateEmptyInput(String userInput) {
         if (userInput.length() == 0) {
@@ -29,11 +30,21 @@ public class Validation {
         }
     }
 
-    public static void validateInputOrder(String userInput) {
+    public static void validateInputString(String userInput) {
         String[] selectFoods = userInput.split(REST);
 
         for (String item : selectFoods) {
             if (!item.trim().contains(DASH)) {
+                ErrorMessage.inputMenuError();
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public static void validateFoodArrayElement(String[] selectFoods) {
+        for (String item : selectFoods) {
+            String[] element = item.split(DASH);
+            if (element.length != ELEMENT_LENGTH) {
                 ErrorMessage.inputMenuError();
                 throw new IllegalArgumentException();
             }
