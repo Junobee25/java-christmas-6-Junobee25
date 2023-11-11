0d87d106f607552b11732d861b9702d0e451169d
Junobee25 <wnsdhqo@naver.com>
2023-11-11 22:04:49 +0900

    feat(controller): 크리스마스 프로모션 컨트롤러 생성

---
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -0,0 +1,15 @@
+package christmas.controller;
+
+import christmas.service.ChristmasService;
+
+public class ChristmasController {
+    private final ChristmasService christmasService;
+
+    public ChristmasController() {
+        christmasService = new ChristmasService();
+    }
+
+    public void decemberPromotion() {
+
+    }
+}
