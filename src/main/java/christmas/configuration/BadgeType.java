154e64638ebfbd3ee5613e539b27b3d64e5ba789
Junobee25 <wnsdhqo@naver.com>
2023-11-12 21:43:09 +0900

    feat(configuration): NONE_BADGE 값 추가

--- src/main/java/christmas/configuration/BadgeType.java
+++ src/main/java/christmas/configuration/BadgeType.java
@@ -3,7 +3,8 @@ package christmas.configuration;
 public enum BadgeType {
     STAR("별", 5000),
     TREE("트리", 10000),
-    SANTA("산타", 2000);
+    SANTA("산타", 2000),
+    NONE_BADGE("없음", 0);
 
     private final String badge;
     private final int benefit;
