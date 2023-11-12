f6029e59bca8eb96b072ff9b01b0087204fbca61
Junobee25 <wnsdhqo@naver.com>
2023-11-12 15:58:36 +0900

    feat(controller): 주문 메뉴 출력 뷰 적용

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -18,7 +18,16 @@ public class ChristmasController {
 
     public void decemberPromotion() {
         int date = inputDate();
-        Map<String, Integer> order = inputOrder();
+        Map<String, Integer> order = inputOrder(date);
+
+        // 날짜입력 유효성 검증까지 완료 나중에 추가 예외케이스 보강
+        // 주문서입력 유효성 검증까지 완료 나중에 추가 예외케이스 보강
+        // 해야되는 것
+        // 총 주문 금액이 10,000이상부터 이벤트가 적용
+        // 음료만 주문시 주문할 수 없음
+        // 메뉴는 한번에 20개까지만 주문할 수 있음
+        // 할인 전 주문 금액이 120000이상이면 증정이벤트
+        // 혜택이 없을 때 없음 출력
     }
 
     private int inputDate() {
@@ -35,12 +44,14 @@ public class ChristmasController {
         }
     }
 
-    public Map<String, Integer> inputOrder() {
+    private Map<String, Integer> inputOrder(int date) {
         InputView.inputViewOrder();
 
         while (true) {
             try {
                 Order order = new Order(getInputOrder());
+                OutputView.outputViewPreviewEvent(date);
+                OutputView.outputViewOrderMenu(order.getOrder());
                 return order.getOrder();
             } catch (IllegalArgumentException e) {
 
