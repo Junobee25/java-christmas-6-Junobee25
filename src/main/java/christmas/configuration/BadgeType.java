96f3e8eb25eb00871dfbc79589bec3911c95308e
Junobee25 <wnsdhqo@naver.com>
2023-11-12 21:38:22 +0900

    feat(configuration): 뱃지 타입 Enum 생성

---
+++ src/main/java/christmas/configuration/BadgeType.java
@@ -0,0 +1,25 @@
+package christmas.configuration;
+
+public enum BadgeType {
+    STAR("별", 5000),
+    TREE("트리", 10000),
+    SANTA("산타", 2000);
+
+    private final String badge;
+    private final int benefit;
+
+    BadgeType(String badge, int benefit) {
+        this.badge = badge;
+        this.benefit = benefit;
+    }
+
+    public String getBadge() {
+        return badge;
+    }
+
+    public int getBenefit() {
+        return benefit;
+    }
+
+
+}
