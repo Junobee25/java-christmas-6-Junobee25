6c6edb07fbb79e35b430036d3badcb0cc49e63a1
Junobee25 <wnsdhqo@naver.com>
2023-11-12 18:37:17 +0900

    refactor(view): 뷰 메서드 수정

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,17 +1,20 @@
 package christmas.view;
 
+import christmas.configuration.GiveWayType;
+import christmas.util.Utils;
 import org.mockito.internal.matchers.Or;
 
 import java.util.Map;
 
 public class OutputView {
     private final static String WELCOME_RESTAURANT = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
-    private final static String PREVIEW_EVENT = "12월 %s일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
+    private final static String PREVIEW_EVENT = "12월 %s일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private final static String ORDER_MENU = "<주문 메뉴>";
     private final static String DETAIL_ORDER = "%s %d개";
     private final static String ORDER_AMOUNT_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
     private final static String DETAIL_ORDER_AMOUNT_BEFORE_DISCOUNT = "%s원";
-    private final static String GIVEAWAY_MENU = "%s %d개";
+    private final static String GIVEAWAY_MENU = "<증정 메뉴>";
+    private final static String GIVEAWAY_MENU_DETAIL = "%s %d개";
     private final static String BENEFIT = "<혜택 내역>";
     private final static String BENEFIT_DETAIL = "%s: -%d원";
     private final static String TOTAL_BENEFIT_DISCOUNT = "<총혜택 금액>";
@@ -20,6 +23,7 @@ public class OutputView {
     private final static String ESTIMATED_PAYMENT_AMOUNT_AFTER_DISCOUNT_DETAIL = "%s원";
     private final static String DECEMBER_EVENT_BADGE = "<12월 이벤트 배지>";
     private final static String DECEMBER_EVENT_BADGE_DETAIL = "%s";
+    private final static String NONE = "없음";
 
     public static void outputViewWelcome() {
         System.out.println(WELCOME_RESTAURANT);
@@ -37,16 +41,21 @@ public class OutputView {
         }
     }
 
-    public static void outputViewOrderAmount() {
+    public static void outputViewOrderAmount(int price) {
         System.out.println(ORDER_AMOUNT_BEFORE_DISCOUNT);
+        System.out.printf(DETAIL_ORDER_AMOUNT_BEFORE_DISCOUNT, Utils.formatPriceToWonType(price));
     }
 
-    public static void outputViewDetailOrderAmount(int price) {
-        System.out.printf(DETAIL_ORDER_AMOUNT_BEFORE_DISCOUNT, price);
-    }
+    public static void outputViewGiveWayMenu(int totalPrice) {
+        System.out.println(GIVEAWAY_MENU);
 
-    public static void outputViewGiveWayMenu(String goods, int count) {
-        System.out.printf(GIVEAWAY_MENU, goods, count);
+        if (totalPrice >= 120000) {
+            System.out.printf(GIVEAWAY_MENU_DETAIL, GiveWayType.GIVE_WAY.getGiveWay(), GiveWayType.GIVE_WAY.getCount());
+        }
+        if (totalPrice < 120000) {
+            System.out.println(NONE);
+        }
+
     }
 
     public static void outputViewBenefit() {
