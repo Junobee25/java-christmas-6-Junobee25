1dd080eab2c49c80589d1a99507ea4917597b1a7
Junobee25 <wnsdhqo@naver.com>
2023-11-13 00:04:51 +0900

    feat(domain): validateOnlyBeverage 음료 만 주문한 경우 추가

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,12 +1,30 @@
 package christmas.domain;
 
-import java.util.Map;
+import christmas.configuration.MenuType;
+import christmas.util.Utils;
+import christmas.view.ErrorMessage;
+
+import java.util.*;
 
 public class Order {
     private final Map<String, Integer> order;
 
     public Order(Map<String, Integer> order) {
+        validateOnlyBeverage(order);
         this.order = order;
+    }
+
+    private void validateOnlyBeverage(Map<String, Integer> order) {
+        HashSet<MenuType> menus = new HashSet<>();
+
+        for (MenuType menu : MenuType.values()) {
+            Utils.findMenuType(menu, order);
+        }
+
+        if (menus.contains(MenuType.BEVERAGE) && menus.size() == 1) {
+            ErrorMessage.inputMenuError();
+            throw new IllegalArgumentException();
+        }
     }
 
     public Map<String, Integer> getOrder() {
