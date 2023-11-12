e67d61d00ceeddfa911e0c4279bd11c6ac777e8d
Junobee25 <wnsdhqo@naver.com>
2023-11-12 15:23:51 +0900

    feat(view): 뷰 메서드 작성

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,7 @@
 package christmas.view;
 
+import org.mockito.internal.matchers.Or;
+
 public class OutputView {
     private final static String WELCOME_RESTAURANT = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private final static String PREVIEW_EVENT = "12월 %s일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
@@ -19,6 +21,62 @@ public class OutputView {
 
     public static void outputViewWelcome() {
         System.out.println(WELCOME_RESTAURANT);
+    }
+
+    public static void outputViewPreviewEvent() {
+        System.out.println(PREVIEW_EVENT);
+    }
+
+    public static void outputViewOrderMenu() {
+        System.out.println(ORDER_MENU);
+    }
+
+    public static void outputViewDetailOrder(String menu, int count) {
+        System.out.printf(DETAIL_ORDER, menu, count);
+    }
+
+    public static void outputViewOrderAmount() {
+        System.out.println(ORDER_AMOUNT_BEFORE_DISCOUNT);
+    }
+
+    public static void outputViewDetailOrderAmount(int price) {
+        System.out.printf(DETAIL_ORDER_AMOUNT_BEFORE_DISCOUNT, price);
+    }
+
+    public static void outputViewGiveWayMenu(String goods, int count) {
+        System.out.printf(GIVEAWAY_MENU, goods, count);
+    }
+
+    public static void outputViewBenefit() {
+        System.out.println(BENEFIT);
+    }
+
+    public static void outputViewDetailBenefit(String event, int discount) {
+        System.out.printf(BENEFIT_DETAIL, event, discount);
+    }
+
+    public static void outputViewTotalDiscount() {
+        System.out.println(TOTAL_BENEFIT_DISCOUNT);
+    }
+
+    public static void outputViewTotalDetailDiscount(int discount) {
+        System.out.printf(TOTAL_BENEFIT_DISCOUNT_DETAIL, discount);
+    }
+
+    public static void outputViewEstimatedPayment() {
+        System.out.println(ESTIMATED_PAYMENT_AMOUNT_AFTER_DISCOUNT);
+    }
+
+    public static void outputViewEstimatedDetailPayment(int totalPrice) {
+        System.out.printf(ESTIMATED_PAYMENT_AMOUNT_AFTER_DISCOUNT_DETAIL, totalPrice);
+    }
+
+    public static void outputViewEventBadge() {
+        System.out.println(DECEMBER_EVENT_BADGE);
+    }
+
+    public static void outputViewDetailEventBadge(String badge) {
+        System.out.printf(DECEMBER_EVENT_BADGE_DETAIL, badge);
     }
 
 }
