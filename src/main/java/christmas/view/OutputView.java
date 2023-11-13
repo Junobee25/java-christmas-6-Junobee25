13c93da8bd0f2dd5853d6ec6fe4ba33c54e403c1
Junobee25 <wnsdhqo@naver.com>
2023-11-13 15:24:34 +0900

    refactor(view): 줄바꿈 수정

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -8,21 +8,21 @@ import java.util.Map;
 
 public class OutputView {
     private final static String WELCOME_RESTAURANT = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
-    private final static String PREVIEW_EVENT = "12월 %s일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    private final static String PREVIEW_EVENT = "12월 %s일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
     private final static String ORDER_MENU = "<주문 메뉴>";
-    private final static String DETAIL_ORDER = "%s %d개";
+    private final static String DETAIL_ORDER = "%s %d개\n";
     private final static String ORDER_AMOUNT_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
-    private final static String DETAIL_ORDER_AMOUNT_BEFORE_DISCOUNT = "%s원";
+    private final static String DETAIL_ORDER_AMOUNT_BEFORE_DISCOUNT = "%s원\n";
     private final static String GIVEAWAY_MENU = "<증정 메뉴>";
-    private final static String GIVEAWAY_MENU_DETAIL = "%s %d개";
+    private final static String GIVEAWAY_MENU_DETAIL = "%s %d개\n";
     private final static String BENEFIT = "<혜택 내역>";
-    private final static String BENEFIT_DETAIL = "%s: -%d원";
+    private final static String BENEFIT_DETAIL = "%s: -%s원\n";
     private final static String TOTAL_BENEFIT_DISCOUNT = "<총혜택 금액>";
-    private final static String TOTAL_BENEFIT_DISCOUNT_DETAIL = "-%s원";
+    private final static String TOTAL_BENEFIT_DISCOUNT_DETAIL = "-%s원\n";
     private final static String ESTIMATED_PAYMENT_AMOUNT_AFTER_DISCOUNT = "<할인 후 예상 결제 금액>";
-    private final static String ESTIMATED_PAYMENT_AMOUNT_AFTER_DISCOUNT_DETAIL = "%s원";
+    private final static String ESTIMATED_PAYMENT_AMOUNT_AFTER_DISCOUNT_DETAIL = "%s원\n";
     private final static String DECEMBER_EVENT_BADGE = "<12월 이벤트 배지>";
-    private final static String DECEMBER_EVENT_BADGE_DETAIL = "%s";
+    private final static String DECEMBER_EVENT_BADGE_DETAIL = "%s\n";
     private final static String NONE = "없음";
 
     public static void outputViewWelcome() {
