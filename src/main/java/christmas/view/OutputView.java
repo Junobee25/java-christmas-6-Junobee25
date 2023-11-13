499e61f3e2a99f73529af11ab20a4d95c257a97d
Junobee25 <wnsdhqo@naver.com>
2023-11-13 16:20:14 +0900

    refactor(view): 이벤트 뷰 수정

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -8,19 +8,19 @@ import java.util.Map;
 
 public class OutputView {
     private final static String WELCOME_RESTAURANT = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
-    private final static String PREVIEW_EVENT = "12월 %s일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
+    private final static String PREVIEW_EVENT = "12월 %s일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n\n";
     private final static String ORDER_MENU = "<주문 메뉴>";
     private final static String DETAIL_ORDER = "%s %d개\n";
     private final static String ORDER_AMOUNT_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
-    private final static String DETAIL_ORDER_AMOUNT_BEFORE_DISCOUNT = "%s원\n";
+    private final static String DETAIL_ORDER_AMOUNT_BEFORE_DISCOUNT = "%s원\n\n";
     private final static String GIVEAWAY_MENU = "<증정 메뉴>";
-    private final static String GIVEAWAY_MENU_DETAIL = "%s %d개\n";
+    private final static String GIVEAWAY_MENU_DETAIL = "%s %d개\n\n";
     private final static String BENEFIT = "<혜택 내역>";
     private final static String BENEFIT_DETAIL = "%s: -%s원\n";
     private final static String TOTAL_BENEFIT_DISCOUNT = "<총혜택 금액>";
-    private final static String TOTAL_BENEFIT_DISCOUNT_DETAIL = "-%s원\n";
+    private final static String TOTAL_BENEFIT_DISCOUNT_DETAIL = "-%s원\n\n";
     private final static String ESTIMATED_PAYMENT_AMOUNT_AFTER_DISCOUNT = "<할인 후 예상 결제 금액>";
-    private final static String ESTIMATED_PAYMENT_AMOUNT_AFTER_DISCOUNT_DETAIL = "%s원\n";
+    private final static String ESTIMATED_PAYMENT_AMOUNT_AFTER_DISCOUNT_DETAIL = "%s원\n\n";
     private final static String DECEMBER_EVENT_BADGE = "<12월 이벤트 배지>";
     private final static String DECEMBER_EVENT_BADGE_DETAIL = "%s\n";
     private final static String NONE = "없음";
@@ -39,6 +39,7 @@ public class OutputView {
         for (Map.Entry<String, Integer> menu : order.entrySet()) {
             System.out.printf(DETAIL_ORDER, menu.getKey(), menu.getValue());
         }
+        System.out.println();
     }
 
     public static void outputViewOrderAmount(int price) {
@@ -54,6 +55,7 @@ public class OutputView {
         }
         if (totalPrice < 120000) {
             System.out.println(NONE);
+            System.out.println();
         }
 
     }
@@ -87,5 +89,22 @@ public class OutputView {
     public static void outputViewEventBadge(String badge) {
         System.out.println(DECEMBER_EVENT_BADGE);
         System.out.printf(DECEMBER_EVENT_BADGE_DETAIL, badge);
+    }
+
+    public static void outputViewNotBenefit() {
+        System.out.println(BENEFIT);
+        System.out.println(NONE);
+        System.out.println();
+    }
+
+    public static void outputViewNotDiscountBenefit() {
+        System.out.println(TOTAL_BENEFIT_DISCOUNT);
+        System.out.println(NONE);
+        System.out.println();
+    }
+
+    public static void outputViewNotBadge() {
+        System.out.println(DECEMBER_EVENT_BADGE);
+        System.out.print(NONE);
     }
 }
