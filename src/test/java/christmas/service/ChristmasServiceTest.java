d21bfd99233f09a7a1af47041d11613bad3e054b
Junobee25 <wnsdhqo@naver.com>
2023-11-14 15:32:58 +0900

    test(service): calculateTotalBenefit 단위 테스트 추가

--- src/test/java/christmas/service/ChristmasServiceTest.java
+++ src/test/java/christmas/service/ChristmasServiceTest.java
@@ -1,6 +1,8 @@
 package christmas.service;
 
 import christmas.util.Utils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -9,10 +11,46 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ChristmasServiceTest {
 
+    private ChristmasService christmasService;
+
+    @BeforeEach
+    void setUp() {
+        christmasService = new ChristmasService();
+    }
+
+    @DisplayName("할인 된 가격 테스트")
     @Test
     void calculateTotalDiscount() {
         //given
-        ChristmasService christmasService = new ChristmasService();
+        String userInput = "티본스테이크-2,바비큐립-1";
+        Map<String, Integer> order = Utils.stringToMap(userInput);
+        int date = 1;
+
+        //when
+        int result1 = christmasService.calculateTotalDiscount(date, order);
+
+        //then
+        assertEquals(result1, 7069);
+    }
+
+    @DisplayName("할인 전 총 구매 가격 테스트")
+    @Test
+    void calculateTotalPurchaseAmount() {
+        //given
+        String userInput = "티본스테이크-2,바비큐립-1";
+        Map<String, Integer> order = Utils.stringToMap(userInput);
+
+        //when
+        int result1 = christmasService.calculateTotalPurchaseAmount(order);
+
+        //then
+        assertEquals(result1, 164000);
+    }
+
+    @DisplayName("혜택 받는 가격 테스트")
+    @Test
+    void calculateTotalBenefit() {
+        //given
         String userInput = "티본스테이크-2,바비큐립-1";
         Map<String, Integer> order = Utils.stringToMap(userInput);
         int date = 1;
@@ -21,22 +59,6 @@ class ChristmasServiceTest {
         int result1 = christmasService.calculateTotalBenefit(date, order);
 
         //then
-        org.assertj.core.api.Assertions.assertThat(result1).isEqualTo(32069);
-
-    }
-
-    @Test
-    void calculateTotalPurchaseAmount() {
-        //given
-        ChristmasService christmasService = new ChristmasService();
-        String userInput = "티본스테이크-2,바비큐립-1";
-        Map<String, Integer> order = Utils.stringToMap(userInput);
-
-        //when
-        int result1 = christmasService.calculateTotalPurchaseAmount(order);
-
-        //then
-        org.assertj.core.api.Assertions.assertThat(result1).isEqualTo(164000);
-
+        assertEquals(result1, 32069);
     }
 }
