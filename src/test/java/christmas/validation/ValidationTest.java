a955c3f55ebef7eb797807a0a0a65bdf0905ae76
Junobee25 <wnsdhqo@naver.com>
2023-11-14 12:39:15 +0900

    test(validation): validateDateEmptyInput 단위 테스트 추가

---
+++ src/test/java/christmas/validation/ValidationTest.java
@@ -0,0 +1,63 @@
+package christmas.validation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidationTest {
+
+    @DisplayName("날짜 입력하지 않으면 예외가 발생한다.")
+    @Test
+    void validateDateEmptyInput() {
+        //given
+        String userInput = "";
+
+        //then
+        assertThrows(IllegalArgumentException.class, () -> {
+            Validation.validateDateEmptyInput(userInput);
+        });
+
+    }
+
+    @Test
+    void validateOrderEmptyInput() {
+    }
+
+    @Test
+    void validateEmptyArray() {
+    }
+
+    @Test
+    void validateDateStringToInteger() {
+    }
+
+    @Test
+    void validateOrderStringToInteger() {
+    }
+
+    @Test
+    void validateRangeDate() {
+    }
+
+    @Test
+    void validateInputString() {
+    }
+
+    @Test
+    void validateFoodArrayElement() {
+    }
+
+    @Test
+    void validateElementFood() {
+    }
+
+    @Test
+    void validateElementCount() {
+    }
+
+    @Test
+    void validateDuplicationElementFood() {
+    }
+}
